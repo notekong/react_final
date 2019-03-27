bdd7acf6a5c4c1be8839cc0e93b41dd2a0205df5
Emmett <estokes91@gmail.com>
2019-03-27 14:19:51 -0700

    added in app skellyboi

---
+++ components/Note.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+
+export default class NoteScreen extends React.Component {
+
+  static navigationOptions = {
+    title: 'Home',
+    headerStyle: {
+      backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#fff',
+    headerTintStyle: {
+      fontWeight: 'bold',
+    },
+  };
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text> This is the notes </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
