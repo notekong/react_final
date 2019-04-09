1be0c5805d2a251e601abcad079b020ef53162ef
Alexander Jones <ajones124@my.bcit.ca>
2019-04-08 18:39:50 -0700

    Treats for me

--- service/serviceInterface.js
+++ service/serviceInterface.js
@@ -1,8 +1,21 @@
 import { db } from '../database';
 
 export const addItem = (username, password) => {
-	db.ref('items/').push({
-		username: username,
-		password: password,
-	});
+
+  // Get a key for a new Post.
+  var newPostKey = db.ref().child('posts').push().key;
+
+  var postData = {
+    username: username,
+    password: password,
+    key: newPostKey
+  };
+
+
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  var updates = {};
+  updates['/items/' + newPostKey] = postData;
+
+  return db.ref().update(updates);
+
 }
