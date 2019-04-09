6006927bb7869e7deddb8d009e0c5af3d12e3ac7
Emmett <estokes91@gmail.com>
2019-04-09 12:49:39 -0700

    got rid of comments

--- service/updateServiceInterface.js
+++ service/updateServiceInterface.js
@@ -1,28 +1,18 @@
-// import { db } from '../database';
+import { db } from '../database';
 
-// export const updateItem = (title, details, uniqueid) => {
+export const updateItem = (title, details, uniqueid) => {
 
-//   var noteArray = []
+  var noteArray = []
 
-//   var postData = {
-//     title: username,
-//     details: password,
-//     uniqueid: newPostKey
-//   };
-
-
-//   // Write the new post's data simultaneously in the posts list and the user's post list.
-//   var updates = {};
-//   updates['/items/' + uniqueid + '/' + noteArray] = postData;
-
-//   return db.ref().update(updates);
+  var postData = {
+    title: username,
+    details: password,
+    uniqueid: newPostKey
+  };
 
 
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  var updates = {};
+  updates['/items/' + uniqueid + '/' + noteArray] = postData;
 
-//   function writeUserData(userId, name, email, imageUrl) {
-//   firebase.database().ref('users/' + userId).set({
-//     username: name,
-//     email: email,
-//     profile_picture : imageUrl
-//   });
-// }
+  return db.ref().update(updates);
